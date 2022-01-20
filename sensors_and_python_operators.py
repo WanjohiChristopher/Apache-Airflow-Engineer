77a074418fed948de056b814267b093d9aaec5f3
WanjohiChristopher <55980747+WanjohiChristopher@users.noreply.github.com>
2022-01-20 08:40:37 +0300

    added file sensor for waiting data

--- sensors_and_python_operators.py
+++ sensors_and_python_operators.py
@@ -1,1 +1,5 @@
-https://academy.astronomer.io/astronomer-certification-apache-airflow-fundamentals-preparation/731426
+# https://academy.astronomer.io/astronomer-certification-apache-airflow-fundamentals-preparation/731426
+
+waiting_for_data=FileSensor(task_id="waiting_for_data",fs_conn_id="fs_default",filepath="my_file.txt")
+
+
